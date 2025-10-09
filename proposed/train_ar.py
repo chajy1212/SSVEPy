42e6ad5514d719d5f2a9f2e5d53c37c17285434c
chajy1212 <chajy1212@naver.com>
2025-10-09 21:16:47 +0900

    #

--- proposed/train_ar.py
+++ proposed/train_ar.py
@@ -308,7 +308,7 @@ def main(args):
         all_itrs.append(best_itr)
 
     # Summary
-    print("\n[Final LOSO]")
+    print("\n[Final]")
     print(f"Mean Acc: {np.mean(all_accs):.5} ± {np.std(all_accs):.5f}")
     print(f"Mean ITR: {np.mean(all_itrs):.4f} ± {np.std(all_itrs):.4f}")
 
